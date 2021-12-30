d86b38cbee6414a04c29606be926e7975b3684b2
Death-Raider <oragimirox@gmail.com>
2021-12-31 02:16:20 +0530

    built static

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ import {
 } from "react-router-dom";
 
 import App from './src/App';
-import About from './src/pages/About'
-import Contact from './src/pages/Contact'
-import Connections from './src/pages/Connections'
-import Projects from './src/pages/Projects'
-import Project1 from './src/pages/Project1'
-import Project2 from './src/pages/Project2'
-import Project3 from './src/pages/Project3'
-import Project4 from './src/pages/Project4'
+
 
 ReactDOM.render(
     <React.StrictMode>
